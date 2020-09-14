2052c2b1519d88800e6724b833cdcf2b9ebd0017
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-09-14 13:38:10 +1000

    Add 3 base layers and circle markers

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,11 +1,4 @@
-// Create map object
-var myMap = L.map("map", {
-	center: [23.6978, 120.9605],
-	zoom: 4
-});
-
 // CREATE BASE LAYERS
-// Streetmap Layer
 var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
@@ -34,11 +27,15 @@ var baseMaps = {
 	"Outdoors": outdoors
 };
 
-// Store our API endpoint inside queryUrl
+// Store our API endpoints
 var earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var tectonicUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+var tectonicUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Grab the data with d3
+// Define arrays to hold earthquake circle markers and tectonic plates boundaries
+var earthquakeMarkers = [];
+var tectonicLines = [];
+
+// Grab the earthquake data with d3
 d3.json(earthquakeUrl).then(function(data) {
 
 	// Create function to color cicles according to earthquake magnitudes
@@ -49,7 +46,7 @@ d3.json(earthquakeUrl).then(function(data) {
 					 d >= 2 ? "rgb(243, 219, 77)" :
 					 d >= 1 ? "rgb(225, 243, 77)" :
 					 					"rgb(183, 243, 77)";
-	}
+	};
 	
 	// Grab the features data
 	var features = data.features;
@@ -61,19 +58,20 @@ d3.json(earthquakeUrl).then(function(data) {
 		var coordinates = features[i].geometry.coordinates;
 
 		// Add circles to map
-		L.circle(
-			[coordinates[1], coordinates[0]], {
-				fillOpacity: 0.75,
-				fillColor: getColor(magnitudes),
-				color: "black",
-				weight: 0.5,
-				radius: magnitudes * 15000
-			}
-		).addTo(myMap)
-		.bindPopup("<h3>" + features[i].properties.place +
-			"</h3><hr><p>" + new Date(features[i].properties.time) + 
-			'<br>' + '[' + coordinates[1] + ', ' + coordinates[0] + ']' + "</p>");
-	}	
+		earthquakeMarkers.push(
+			L.circle(
+				[coordinates[1], coordinates[0]], {
+					fillOpacity: 0.75,
+					fillColor: getColor(magnitudes),
+					color: getColor(magnitudes),
+					stroke: false,
+					radius: magnitudes * 17000
+				}
+			).addTo(myMap)
+			.bindPopup("<h3>" + features[i].properties.place +
+				"</h3><hr><p>" + new Date(features[i].properties.time) + 
+				'<br>' + '[' + coordinates[1] + ', ' + coordinates[0] + ']' + "</p>"));
+	};	
 
 	// Legend for the chart
 	var legend = L.control({position: 'bottomright'});
@@ -93,3 +91,26 @@ d3.json(earthquakeUrl).then(function(data) {
 	};
 	legend.addTo(myMap);
 });
+
+// Create two separate layer groups: one for earthquakes and one for tectonic boundaries
+var earthquake = L.layerGroup(earthquakeMarkers);
+// var cities = L.layerGroup(cityMarkers);
+
+// Create an overlay object
+var overlayMaps = {
+  "Earthquakes": earthquake,
+  // "City Population": cities
+};
+
+// Define a map object
+var myMap = L.map("map", {
+	center: [23.6978, 120.9605],
+	zoom: 4,
+  layers: [satellite, earthquake] //this is in order of layering
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
